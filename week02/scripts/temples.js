7c43fe1f99092acda54c78d5c128d94d0bb0dc93
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-05-02 20:46:43 -0600

    Made big progress with the HTML and CSS of the project. Do JS next

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -5,3 +5,5 @@ year.innerHTML = today.getFullYear();
 const lastModified = document.querySelector('#lastModified');
 lastModified.innerHTML = (document.lastModified);
 
+const hamButton = document.querySelector("#hamburger");
+hamButton.addEventListener("click", ()=>{})
