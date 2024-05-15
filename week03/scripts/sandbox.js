f4a2edd8980672132f95b07ba274777c1e90df38
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-05-14 20:00:26 -0600

    Did sandbox work for quiz

--- week03/scripts/sandbox.js
+++ week03/scripts/sandbox.js
@@ -1,21 +1,31 @@
-let scores = [100, 72, 83, 94, 88, 87];
+// let scores = [100, 72, 83, 94, 88, 87];
 
-let new_score = scores.length - 1;
-// let new_score = scores.length;
-// let new_score = scores.lastIndex;
-// let new_score = scores.lastIndex();
-console.log(new_score);
+// let new_score = scores.length - 1;
+// // let new_score = scores.length;
+// // let new_score = scores.lastIndex;
+// // let new_score = scores.lastIndex();
+// console.log(new_score);
 
-let accumulator = 0;
-let count = 0;
-scores.forEach(score => {
-  if (score > 87) {
-    accumulator += score;
-    count ++;
-  }
-});
-if ( count > 0 ) {
-  console.log(accumulator/count); 
-} else {
-  console.log("No scores reported.");
-}
+// let accumulator = 0;
+// let count = 0;
+// scores.forEach(score => {
+//   if (score > 87) {
+//     accumulator += score;
+//     count ++;
+//   }
+// });
+// if ( count > 0 ) {
+//   console.log(accumulator/count); 
+// } else {
+//   console.log("No scores reported.");
+// }
+
+// const countries = ["Canada", "Chili", "Colombia", "Croatia", "Costa Rica"];
+// const countriesLong = countries.filter((country) => country.length > 7);
+// console.log(countriesLong);
+
+let esomeprazole = {'id':'mnb78932','amount':23145,'amountType':'ct','expDate':'10/01/2021'};
+// let dose = esomeprazole['amount'];
+// const dose = esomeprazole[0].amount[1];
+let dose = amount;
+console.log(dose);
